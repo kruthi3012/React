41e89f0b3ae99ace4f9c271886f49fd4dcd699b4
kruthi <kruthi5483@gmail.com>
2022-06-17 13:22:05 +0530

    complex states challenge

---
+++ Day 4/changing-complex-state-practice/src/components/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prev) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prev.lName,
+          email: prev.email
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prev.fName,
+          lName: value,
+          email: prev.email
+        };
+      } else {
+        return {
+          fName: prev.fName,
+          lName: prev.lName,
+          email: value
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          value={contact.fName}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          value={contact.lName}
+          name="lName"
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          value={contact.email}
+          name="email"
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
